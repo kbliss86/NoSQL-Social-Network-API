2129f7c169b6f2e7fec89c59078fbfc64a6f8bd0
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-10-07 00:02:11 -0500

    added code for models and installed the 'moment' npm module for date formating

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+//Reaction.js mongoose model
+const { Schema, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //use moment node package to format date
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports = ReactionSchema;
